63657ba4772ebb7c3ebcb84d597417c96f258ab5
skyADMIN <546485299@qq.com>
2016-03-24 15:23:31 +0800

    init

---
+++ src/main/java/com/lihuanyu/informationquerysystem/InformationquerysystemApplication.java
@@ -0,0 +1,12 @@
+package com.lihuanyu.informationquerysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationquerysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationquerysystemApplication.class, args);
+	}
+}
