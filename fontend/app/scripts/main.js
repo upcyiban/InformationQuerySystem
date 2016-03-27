7dcaa97c2737249b71ebe59d48784fa4a170e4b0
skyADMIN <546485299@qq.com>
2016-03-27 13:54:29 +0800

    前端施工中。

--- fontend/app/scripts/main.js
+++ fontend/app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+  $('.masthead')
+    .visibility({
+      once: false,
+      onBottomPassed: function() {
+        $('.fixed.menu').transition('fade in');
+      },
+      onBottomPassedReverse: function() {
+        $('.fixed.menu').transition('fade out');
+      }
+    })
+  ;
+
+  // create sidebar and attach to menu open
+  $('.ui.sidebar')
+    .sidebar('attach events', '.toc.item')
+  ;
+});
