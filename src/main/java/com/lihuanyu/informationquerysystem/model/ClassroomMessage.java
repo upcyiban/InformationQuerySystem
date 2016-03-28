f3d4c3dfe57782f980f738c14c04a3c85e32fe31
skyADMIN <546485299@qq.com>
2016-03-28 19:46:43 +0800

    修改教室模型。

--- src/main/java/com/lihuanyu/informationquerysystem/model/ClassroomMessage.java
+++ src/main/java/com/lihuanyu/informationquerysystem/model/ClassroomMessage.java
@@ -5,67 +5,11 @@ package com.lihuanyu.informationquerysystem.model;
  * Created by skyADMIN on 16/3/27.
  */
 public class ClassroomMessage {
-    private boolean allNull;
-    private String njLists;
-    private String ntLists;
-    private String dhLists;
-    private String xhLists;
-    private String dlLists;
-    private String xlLists;
-
-    public boolean isAllNull() {
-        return allNull;
-    }
-
-    public void setAllNull(boolean allNull) {
-        this.allNull = allNull;
-    }
-
-    public String getNjLists() {
-        return njLists;
-    }
-
-    public void setNjLists(String njLists) {
-        this.njLists = njLists;
-    }
-
-    public String getNtLists() {
-        return ntLists;
-    }
-
-    public void setNtLists(String ntLists) {
-        this.ntLists = ntLists;
-    }
-
-    public String getDhLists() {
-        return dhLists;
-    }
-
-    public void setDhLists(String dhLists) {
-        this.dhLists = dhLists;
-    }
-
-    public String getXhLists() {
-        return xhLists;
-    }
-
-    public void setXhLists(String xhLists) {
-        this.xhLists = xhLists;
-    }
-
-    public String getDlLists() {
-        return dlLists;
-    }
-
-    public void setDlLists(String dlLists) {
-        this.dlLists = dlLists;
-    }
-
-    public String getXlLists() {
-        return xlLists;
-    }
-
-    public void setXlLists(String xlLists) {
-        this.xlLists = xlLists;
-    }
+    public boolean allNull;
+    public String njLists;
+    public String ntLists;
+    public String dhLists;
+    public String xhLists;
+    public String dlLists;
+    public String xlLists;
 }
