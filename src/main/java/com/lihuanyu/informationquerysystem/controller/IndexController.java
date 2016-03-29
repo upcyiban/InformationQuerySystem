483533bfaec20345bd5f72b02090c51df5de6a56
skyADMIN <546485299@qq.com>
2016-03-29 20:10:22 +0800

    自习室控制器修改

--- src/main/java/com/lihuanyu/informationquerysystem/controller/IndexController.java
+++ src/main/java/com/lihuanyu/informationquerysystem/controller/IndexController.java
@@ -28,14 +28,27 @@ public class IndexController {
         return loginService.processAuth(verify_request);
     }
 
+    @RequestMapping(value = "/available", method = RequestMethod.POST)
+    public String findAvailableClassByTime(String week, String day, String n, Model model) {
+        QueryUtil queryUtil = new QueryUtil();
+        ClassroomMessage classroomMessage;
+        if (week.equals("0") || day.equals("0") || n.equals("0")) {
+            classroomMessage = availableClassroomService.processMessage(queryUtil.findCurrentAvailableClassroom());
+        } else {
+            classroomMessage = availableClassroomService.processByTimeMessage(queryUtil.findAvailableClassroomByTime(week, day, n));
+        }
+        model.addAttribute("classmessage", classroomMessage);
+        return "index";
+    }
+
     @RequestMapping("/")
-    public String showIndex(Model model){
-        if (!loginService.isLogin()){
+    public String showIndex(Model model) {
+        if (!loginService.isLogin()) {
             return "redirect:/yibanauth";
-        }else {
+        } else {
             QueryUtil queryUtil = new QueryUtil();
             ClassroomMessage classroomMessage = availableClassroomService.processMessage(queryUtil.findCurrentAvailableClassroom());
-            model.addAttribute("classmessage",classroomMessage);
+            model.addAttribute("classmessage", classroomMessage);
             return "index";
         }
     }
