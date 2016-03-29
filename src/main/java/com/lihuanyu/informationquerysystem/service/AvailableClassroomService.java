94bb1ed1cd12e5ed6901a67fdbf5d89b8a66ab6c
skyADMIN <546485299@qq.com>
2016-03-29 09:14:07 +0800

    查教室service的修改

--- src/main/java/com/lihuanyu/informationquerysystem/service/AvailableClassroomService.java
+++ src/main/java/com/lihuanyu/informationquerysystem/service/AvailableClassroomService.java
@@ -12,24 +12,30 @@ public class AvailableClassroomService {
 
     QueryUtil queryUtil = new QueryUtil();
 
-    public ClassroomMessage processMessage(String message){
+    public ClassroomMessage processMessage(String message) {
         ClassroomMessage classroomMessage = new ClassroomMessage();
-        if (message.equals("所有教室当前均无课")){
-            classroomMessage.setAllNull(true);
+        if (message.equals("所有教室当前均无课")) {
+            classroomMessage.njLists = "均可自习";
+            classroomMessage.ntLists = "均可自习";
+            classroomMessage.dhLists = "均可自习";
+            classroomMessage.xhLists = "均可自习";
+            classroomMessage.dlLists = "均可自习";
+            classroomMessage.xlLists = "均可自习";
+            classroomMessage.allNull = true;
             return classroomMessage;
-        }else {
-            String[] result = message.split("： |。 ");
-            classroomMessage.setNjLists(result[1]);
-            classroomMessage.setNtLists(result[3]);
-            classroomMessage.setDhLists(result[5]);
-            classroomMessage.setXhLists(result[7]);
-            classroomMessage.setDlLists(result[9]);
-            classroomMessage.setXlLists(result[11]);
+        } else {
+            String[] result = message.split("：|。");
+            classroomMessage.njLists = result[1];
+            classroomMessage.ntLists = result[3];
+            classroomMessage.dhLists = result[5];
+            classroomMessage.xhLists = result[7];
+            classroomMessage.dlLists = result[9];
+            classroomMessage.xlLists = result[11];
             return classroomMessage;
         }
     }
 
-    public String getMessage(){
+    public String getMessage() {
         return queryUtil.findCurrentAvailableClassroom();
     }
 }
