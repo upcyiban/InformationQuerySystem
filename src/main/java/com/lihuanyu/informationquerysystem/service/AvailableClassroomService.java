8e799a7285d2d73a5897a03f74d9c5d914d61792
skyADMIN <546485299@qq.com>
2016-03-29 20:10:38 +0800

    业务层修改

--- src/main/java/com/lihuanyu/informationquerysystem/service/AvailableClassroomService.java
+++ src/main/java/com/lihuanyu/informationquerysystem/service/AvailableClassroomService.java
@@ -14,28 +14,46 @@ public class AvailableClassroomService {
 
     public ClassroomMessage processMessage(String message) {
         ClassroomMessage classroomMessage = new ClassroomMessage();
-        if (message.equals("所有教室当前均无课")) {
-            classroomMessage.njLists = "均可自习";
-            classroomMessage.ntLists = "均可自习";
-            classroomMessage.dhLists = "均可自习";
-            classroomMessage.xhLists = "均可自习";
-            classroomMessage.dlLists = "均可自习";
-            classroomMessage.xlLists = "均可自习";
-            classroomMessage.allNull = true;
-            return classroomMessage;
-        } else {
-            String[] result = message.split("：|。");
-            classroomMessage.njLists = result[1];
-            classroomMessage.ntLists = result[3];
-            classroomMessage.dhLists = result[5];
-            classroomMessage.xhLists = result[7];
-            classroomMessage.dlLists = result[9];
-            classroomMessage.xlLists = result[11];
-            return classroomMessage;
-        }
+        classroomMessage.currentTime = "当前可自习的教室";
+        return tidyMessage(classroomMessage, message);
     }
 
     public String getMessage() {
         return queryUtil.findCurrentAvailableClassroom();
+    }
+
+    public ClassroomMessage processByTimeMessage(String message) {
+        ClassroomMessage classroomMessage = new ClassroomMessage();
+        String[] resone = message.split("课,");
+        classroomMessage.currentTime = resone[0];
+        String middleMessage = resone[1];
+        return tidyMessage(classroomMessage, middleMessage);
+    }
+
+    /**
+     * 整理自习室信息
+     * @param c
+     * @param m
+     * @return
+     */
+    public ClassroomMessage tidyMessage(ClassroomMessage c, String m) {
+        if (m.equals("所有教室当前均无课")) {
+            c.njLists = "均可自习";
+            c.ntLists = "均可自习";
+            c.dhLists = "均可自习";
+            c.xhLists = "均可自习";
+            c.dlLists = "均可自习";
+            c.xlLists = "均可自习";
+            c.allNull = true;
+        } else {
+            String[] result = m.split("：|。");
+            c.njLists = result[1];
+            c.ntLists = result[3];
+            c.dhLists = result[5];
+            c.xhLists = result[7];
+            c.dlLists = result[9];
+            c.xlLists = result[11];
+        }
+        return c;
     }
 }
