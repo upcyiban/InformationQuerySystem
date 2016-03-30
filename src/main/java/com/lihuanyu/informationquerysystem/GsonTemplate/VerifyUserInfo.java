390989ee1facc73349a87e21ae5536621823f846
skyADMIN <546485299@qq.com>
2016-03-30 16:21:29 +0800

    需要用到的信息

--- src/main/java/com/lihuanyu/informationquerysystem/GsonTemplate/VerifyUserInfo.java
+++ src/main/java/com/lihuanyu/informationquerysystem/GsonTemplate/VerifyUserInfo.java
@@ -4,4 +4,17 @@ package com.lihuanyu.informationquerysystem.GsonTemplate;
  * Created by skyADMIN on 16/3/30.
  */
 public class VerifyUserInfo {
+    public String status;
+    public RealInfo info;
+
+    public class RealInfo {
+        public int yb_userid;
+        public String yb_realname;
+        public String yb_schoolid;
+        public String yb_schoolname;
+        public String yb_studentid;
+        public String code;
+        public String msgCN;
+        public String msgEN;
+    }
 }
