3df6696d5417a43ebf1f2aca67d385c3946dff35
skyADMIN <546485299@qq.com>
2016-03-30 18:35:26 +0800

    解析接口返回的成绩json数据

---
+++ src/main/java/com/lihuanyu/informationquerysystem/GsonTemplate/ScoreJsonInfo.java
@@ -0,0 +1,17 @@
+package com.lihuanyu.informationquerysystem.GsonTemplate;
+
+import java.util.ArrayList;
+
+/**
+ * Created by skyADMIN on 16/3/30.
+ */
+public class ScoreJsonInfo {
+
+    public ArrayList<ScoreInfo> Table;
+
+    public class ScoreInfo{
+        public String XQM;
+        public int KCCJ;
+        public String KCMC;
+    }
+}
