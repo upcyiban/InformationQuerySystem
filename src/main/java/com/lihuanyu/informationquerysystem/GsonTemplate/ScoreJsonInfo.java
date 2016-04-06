1ac3c23480b6e1a83ef222935ef23b3d21de0b64
skyADMIN <546485299@qq.com>
2016-04-06 12:47:45 +0800

    重写了Score类的dashcode方法和equals方法。以让hashest判断俩对象是否相等

--- src/main/java/com/lihuanyu/informationquerysystem/GsonTemplate/ScoreJsonInfo.java
+++ src/main/java/com/lihuanyu/informationquerysystem/GsonTemplate/ScoreJsonInfo.java
@@ -9,9 +9,34 @@ public class ScoreJsonInfo {
 
     public ArrayList<ScoreInfo> Table;
 
-    public class ScoreInfo{
+    public class ScoreInfo implements Comparable{
         public String XQM;
         public int KCCJ;
         public String KCMC;
+
+        @Override
+        public int hashCode() {
+            final int PRIME = 31;
+            int result = 1;
+            result = PRIME * result + KCCJ;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+//            if (KCMC.equals(((ScoreInfo)obj).KCMC) && KCCJ == ((ScoreInfo) obj).KCCJ)
+//                return true;
+//            else
+//                return false;
+            return KCMC.equals(((ScoreInfo)obj).KCMC);
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if (KCMC.equals(((ScoreInfo)o).KCMC) && KCCJ == ((ScoreInfo) o).KCCJ)
+                return 0;
+            else
+                return 1;
+        }
     }
 }
