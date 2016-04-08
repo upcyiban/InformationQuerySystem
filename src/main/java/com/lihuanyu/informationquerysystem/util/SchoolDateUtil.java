657e22048792cb864359207690630bb9d8bf85d1
skyADMIN <546485299@qq.com>
2016-04-08 17:01:58 +0800

    获取校历周数、星期的类和其测试类

---
+++ src/main/java/com/lihuanyu/informationquerysystem/util/SchoolDateUtil.java
@@ -0,0 +1,15 @@
+package com.lihuanyu.informationquerysystem.util;
+
+/**
+ * Created by skyADMIN on 16/4/8.
+ */
+public class SchoolDateUtil {
+
+
+
+    public int getWeek(){
+        return 0;
+    }
+
+
+}
---
+++ src/test/java/com/lihuanyu/informationquerysystem/util/SchoolDateTest.java
@@ -0,0 +1,19 @@
+package com.lihuanyu.informationquerysystem.util;
+
+import com.lihuanyu.informationquerysystem.InformationquerysystemApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by skyADMIN on 16/4/8.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = InformationquerysystemApplication.class)
+@WebAppConfiguration
+public class SchoolDateTest {
+
+
+
+}
