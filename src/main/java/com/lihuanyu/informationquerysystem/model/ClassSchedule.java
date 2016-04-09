7ae0ee0ca1e58b873be334f9d5349885d358866b
skyADMIN <546485299@qq.com>
2016-04-09 10:35:17 +0800

    课程表模型

---
+++ src/main/java/com/lihuanyu/informationquerysystem/model/ClassSchedule.java
@@ -0,0 +1,12 @@
+package com.lihuanyu.informationquerysystem.model;
+
+/**
+ * Created by skyADMIN on 16/4/8.
+ */
+public class ClassSchedule {
+
+    public String coursesName;
+    public String coursesLocation;
+    public String coursesTime;
+
+}
