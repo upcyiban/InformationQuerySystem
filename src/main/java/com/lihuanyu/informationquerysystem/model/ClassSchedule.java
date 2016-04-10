e8bb5216c396248b75e443803fa73de5277281ef
skyADMIN <546485299@qq.com>
2016-04-10 22:58:37 +0800

    重写equals方法

--- src/main/java/com/lihuanyu/informationquerysystem/model/ClassSchedule.java
+++ src/main/java/com/lihuanyu/informationquerysystem/model/ClassSchedule.java
@@ -9,4 +9,8 @@ public class ClassSchedule {
     public String coursesLocation;
     public String coursesTime;
 
+    @Override
+    public boolean equals(Object obj) {
+        return (coursesName.equals(((ClassSchedule)obj).coursesName) && coursesLocation.equals(((ClassSchedule)obj).coursesLocation) && coursesTime.equals(((ClassSchedule)obj).coursesTime));
+    }
 }
