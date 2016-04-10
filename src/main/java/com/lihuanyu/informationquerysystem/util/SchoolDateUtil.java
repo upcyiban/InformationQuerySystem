2368f3983c07889c0849a1fce8b1bc85ded5245d
echaoo <1299529160@qq.com>
2016-04-10 20:39:03 +0800

    修复周数和星期获取不正常的bug

--- src/main/java/com/lihuanyu/informationquerysystem/util/SchoolDateUtil.java
+++ src/main/java/com/lihuanyu/informationquerysystem/util/SchoolDateUtil.java
@@ -8,15 +8,21 @@ import java.util.Date;
  * Created by skyADMIN on 16/4/8.
  */
 public class SchoolDateUtil {
-    public long getTime(){
+
+    private long getCurrentTime(){
         Date currentTime = new Date();
         long current = currentTime.getTime();
-        long time = 0;
+        return current;
+    }
+
+    private double getTime(){
+        long current = getCurrentTime();
+        double time = 0;
         long begin = 0;
         try {
             SimpleDateFormat beginTime = new SimpleDateFormat("yyyy-MM-dd");
             begin = beginTime.parse("2016-02-28").getTime();
-            time = current- begin;
+            time = current - begin;
         }catch (ParseException e){
             e.printStackTrace();
         }
@@ -24,23 +30,45 @@ public class SchoolDateUtil {
     }
 
     public int getWeek(){
-        int week =0;
-        double days = Math.ceil(getTime()/86400000);
+        int week = 0;
+        double days = Math.ceil(getTime()/86400000.0);
         week = (int) Math.ceil(days/7);//第几周
         return week;
     }
 
     public int getDay(){
-        int day =0;
-        double days = Math.ceil(getTime()/86400000);
+        int day = 0;
+        double days = Math.ceil(getTime()/86400000.0);
         switch ((int)Math.ceil(days)%7){
-            case 0: day = 7;break;
-            case 1: day = 1;break;
-            case 2: day = 2;break;
-            case 3: day = 3;break;
-            case 4: day = 4;break;
-            case 5: day = 5;break;
-            case 6: day = 6;break;
+            case 0: day = 6;break;
+            case 1: day = 7;break;
+            case 2: day = 1;break;
+            case 3: day = 2;break;
+            case 4: day = 3;break;
+            case 5: day = 4;break;
+            case 6: day = 5;break;
+        }//得到当天是星期几
+        return day;
+    }
+
+    public int getWeek(double time){
+        int week = 0;
+        double days = Math.ceil(time/86400000.0);
+        week = (int) Math.ceil(days/7);//第几周
+        return week;
+    }
+
+    public int getDay(double time){
+        int day = 0;
+        double days = Math.ceil(time/86400000.0);
+        switch ((int)Math.ceil(days)%7){
+            case 0: day = 6;break;
+            case 1: day = 7;break;
+            case 2: day = 1;break;
+            case 3: day = 2;break;
+            case 4: day = 3;break;
+            case 5: day = 4;break;
+            case 6: day = 5;break;
         }//得到当天是星期几
         return day;
     }
--- src/test/java/com/lihuanyu/informationquerysystem/util/SchoolDateTest.java
+++ src/test/java/com/lihuanyu/informationquerysystem/util/SchoolDateTest.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by skyADMIN on 16/4/8.
  */
@@ -18,12 +21,26 @@ public class SchoolDateTest {
     SchoolDateUtil schoolDateUtil = new SchoolDateUtil();
 
     @Test
-    public void testGetWeek(){
-        System.out.println(schoolDateUtil.getWeek());
+    public void testGetWeek() throws ParseException {
+        SimpleDateFormat beginTime = new SimpleDateFormat("yyyy-MM-dd");
+        long begin = beginTime.parse("2016-02-28").getTime();
+        long current = beginTime.parse("2016-04-01").getTime();
+        System.out.println(" ");
+        //System.out.println(current);
+        //System.out.println(schoolDateUtil.getCurrentTime());
+        double time = current - begin + 1;
+        System.out.println(schoolDateUtil.getWeek(time));
     }
 
     @Test
-    public void testGetDay(){
-        System.out.println(schoolDateUtil.getDay());
+    public void testGetDay() throws ParseException {
+        SimpleDateFormat beginTime = new SimpleDateFormat("yyyy-MM-dd");
+        long begin = beginTime.parse("2016-02-28").getTime();
+        long current = beginTime.parse("2016-03-09").getTime();
+        System.out.println(" ");
+        //System.out.println(current);
+        //System.out.println(schoolDateUtil.getCurrentTime());
+        double time = current - begin + 1;
+        System.out.println(schoolDateUtil.getDay(time));
     }
 }
