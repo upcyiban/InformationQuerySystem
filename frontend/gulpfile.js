577693bdf7695bf6e82a78d10129378ef3616f7c
jaxlying <jaxlying@outlook.com>
2016-10-30 11:12:33 +0800

    fix can not load icon

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -82,6 +82,7 @@ gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
     .concat('app/fonts/**/*'))
     .pipe(gulp.dest('.tmp/fonts'))
+    .pipe(gulp.dest('dist/styles/themes/default/assets/fonts/'))
     .pipe(gulp.dest('dist/fonts'));
 });
 
